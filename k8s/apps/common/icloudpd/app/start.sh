335c2d792bd3ddd7790f2a67c406ba0b7e97f1d9
shelken <shelken.pxk@gmail.com>
2025-08-26 11:21:02 +0800

    fix: icloudpd start script

--- k8s/apps/common/icloudpd/app/start.sh
+++ k8s/apps/common/icloudpd/app/start.sh
@@ -1,24 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/sh
+APPLE_ID=$(cat /config/secret/APPLE_ID)
+
 APPLE_ID=$(cat /config/secret/APPLE_ID)
 
 # ref: https://icloud-photos-downloader.github.io/icloud_photos_downloader/reference.html
 # ref: https://github.com/icloud-photos-downloader/icloud_photos_downloader/blob/master/src/icloudpd/base.py
-args=(
-  # "debug", "info", "error"
-  --log-level=debug
-  --directory=/data
-  --cookie-directory=/config
-  --username="$APPLE_ID"
-  --domain=cn
-  --auto-delete
-  --no-progress-bar
-  --size=original
-  --keep-unicode-in-filenames=true
-  --folder-structure="{:%Y/%m/%d}"
-  # 21600=6h
-  --watch-with-interval=21600
-  --mfa-provider=webui
-  --password-provider=webui
-)
 
-/app/icloudpd_ex icloudpd "${args[@]}"
+/app/icloudpd_ex icloudpd \
+  ## "debug", "info", "error"
+  --log-level=debug \
+  --directory=/data \
+  --cookie-directory=/config \
+  --username="$APPLE_ID" \
+  --domain=cn \
+  --auto-delete \
+  --no-progress-bar \
+  --size=original \
+  --keep-unicode-in-filenames=true \
+  --folder-structure="{:%Y/%m/%d}" \
+  ## 21600=6h
+  --watch-with-interval=21600 \
+  --mfa-provider=webui \
+  --password-provider=webui
