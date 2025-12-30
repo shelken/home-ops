c834f2ff33ab9ea8a9114be3811d3071e633c337
shelken <shelken.pxk@gmail.com>
2025-12-30 12:47:01 +0800

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Base development template";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils = {
       url = "github:numtide/flake-utils";
     };
